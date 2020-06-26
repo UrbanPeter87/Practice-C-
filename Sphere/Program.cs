b4a70913e8b813ed4a8edc0a7b32b06e141a8fbd
Urban Peter <urbanpetertundra87@gmail.com>
2020-06-26 16:48:22 +0200

    sphere

---
+++ Sphere/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Add meg a gömbsugarát: ");
+            int radius = int.Parse(Console.ReadLine());
+
+            int surface = (int)(4 * (radius * radius) * 3.14);
+            int volume = (int)((4 * (radius * radius * radius) * 3.14) / 3);
+
+            Console.WriteLine("A gömb felszine: " + surface);
+            Console.WriteLine("A gömb térfogata: " + volume);
+
+            Console.ReadLine();
+
+        }
+    }
+}
