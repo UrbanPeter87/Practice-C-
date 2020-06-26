bc94fffec6d22fa8a1ea99e1a41cd37c4c24d662
Urban Peter <urbanpetertundra87@gmail.com>
2020-06-26 16:44:06 +0200

    C#Game

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random random = new Random();
+            int computerNumber = random.Next(1, 11);
+
+            int answerCounter = 0;
+
+            bool match = false;
+
+            while (!match && (answerCounter < 10))
+            {
+                Console.Write("Tippelj: ");
+                int myNumber = int.Parse(Console.ReadLine());
+                answerCounter++;
+                if (myNumber == computerNumber)
+                {
+                    Console.WriteLine("Kitaláltad!");
+                    match = true;
+                }
+                else if (computerNumber < myNumber)
+                {
+                    Console.WriteLine("Nem talált! Kisebbre gondoltam.");
+                    Console.WriteLine($"Ez volt a {answerCounter}. ötleted.");
+                }
+                else if (computerNumber > myNumber)
+                {
+                    Console.WriteLine("Nem talált! Nagyobbra gondoltam.");
+                    Console.WriteLine($"Ez volt a {answerCounter}. ötleted");
+                }
+            }
+
+        }
+    }
+}
