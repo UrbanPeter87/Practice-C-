8aa54bc5fd8c463edf5619ef7354dae435fd1753
Urban Peter <urbanpetertundra87@gmail.com>
2020-06-26 16:52:05 +0200

    numberlist

---
+++ NumberList/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Dynamic;
+
+namespace ConsoleApp4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> negative = new List<int>();
+            List<int> positive = new List<int>();
+
+            int numberCounter = 0;
+
+            while (numberCounter < 10)
+            {
+                Console.WriteLine("Adj meg egy számot: ");
+                int number = int.Parse(Console.ReadLine());
+                numberCounter++;
+                if (number > 0)
+                {   
+                    positive.Add(number);
+                }
+                else if (number < 0)
+                {
+                    negative.Add(number);
+                }
+                if (numberCounter == 10)
+                {
+                    Console.WriteLine("Pozitív számok száma: " + positive.Count);
+                    Console.WriteLine("Negatív számok száma: " + negative.Count);
+                }
+            }
+        }
+    }
+}
