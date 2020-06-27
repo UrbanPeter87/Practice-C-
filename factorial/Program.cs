b8ac4d0adc1f17ef06538a9ee554807118875741
Urban Peter <urbanpetertundra87@gmail.com>
2020-06-27 14:57:35 +0200

    factorial

---
+++ factorial/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace ConsoleApp3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Kérem adjon meg egy pozitív egész számot:");
+            uint number = uint.Parse(Console.ReadLine());
+
+            long factorial = number;
+
+            for (uint i = number; i > 1; i--)
+            {
+                factorial *= i - 1;
+            }
+            Console.WriteLine(number + "! = " + factorial);
+        }
+    }
+}
