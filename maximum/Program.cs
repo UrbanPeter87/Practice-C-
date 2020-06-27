50b76db84cacef81869aed5baebb2a58d0b563cd
Urban Peter <urbanpetertundra87@gmail.com>
2020-06-27 15:12:12 +0200

    max

---
+++ maximum/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace ConsoleApp3
+{
+    class Program
+    {
+        static int IndexOfBestWeapon(int[] weaponData)
+        {
+            int bestIndex = 0;
+            
+            for (int i = 1; i < weaponData.Length; i++)
+            {
+                if(weaponData[i] > weaponData[bestIndex])
+                {
+                    bestIndex = i;
+                }
+            }
+
+            return bestIndex;
+        }
+
+        static void Main(string[] args)
+        {
+            int[] damages = { 20, 50, 10, 200, 50, 140 };
+
+            Console.WriteLine("A legjobb fegyver: {0}", IndexOfBestWeapon(damages));
+        }
+    }
+}
